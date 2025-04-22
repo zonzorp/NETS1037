d82e8e74fbd22150ab93d1e65cf361e59a33da80
Dennis Simpson <dennis@zonzorp.net>
2025-04-22 17:38:46 -0400

    Update makecontainers.sh

--- makecontainers.sh
+++ makecontainers.sh
@@ -184,9 +184,10 @@ echo "You may ignore any messages about Open vSwitch or dpkg-preconfigure being 
 echo "DO NOT use control-Z when this script is running."
 
 # ensure we have ssh keys
-echoverbose "Setting up SSH keys for $(id -un)"
-[ -f ~/.ssh/id_ed25519.pub ] && ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" > /dev/null
-
+if [ ! -f ~/.ssh/id_ed25519.pub ]; then
+	echoverbose "Setting up SSH keys for $(id -un)"
+	ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" > /dev/null
+fi
 # init incus if no incusbr0 exists yet, else get rid of old containers if fresh is requested
 if ! ip a s incusbr0 >&/dev/null; then
   echoverbose "Initializing incus"
