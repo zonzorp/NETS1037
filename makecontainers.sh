86d55b584d5bff5d9ad18dc26e6b46a63c49af90
Dennis Simpson <dennis@zonzorp.net>
2025-04-22 18:08:32 -0400

    Update makecontainers.sh

--- makecontainers.sh
+++ makecontainers.sh
@@ -447,7 +447,7 @@ if [ "$nets1037" = "true" ]; then
 		webhost )
 			echoverbose "Doing $container specific setup"
 			# software installs first
-   			if ! packageinstalls "$container" postfix dovecot-imapd mailutils apache2 roundcube; then
+   			if ! packageinstalls "$container" apache2; then
 	  			echoverbose "Package installs failed, leaving $container unconfigured"
 	  			continue
 	  		fi
