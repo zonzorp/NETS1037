004444df0cad77f99f1c2d7f8ec648d4536b1c78
Dennis Simpson <dennis@zonzorp.net>
2025-05-31 09:43:03 -0400

    Update Lab02-loghost-checks.sh

--- Labs/Lab02-loghost-checks.sh
+++ Labs/Lab02-loghost-checks.sh
@@ -51,30 +51,3 @@ for host in loghost mailhost webhost proxyhost nmshost; do
   fi
 done
 echo "Score: $score"
-
-exit
-
-echo Host config checks
-echo ===================
-echo $(hostname) has IP $(hostname -I)
-echo ===================
-
-echo rsyslog/mysql checks
-echo ===================
-echo -n "record count in SystemEvents table: "
-sudo mysql -u root  <<< "select count(*) from Syslog.SystemEvents;"
-echo ===================
-
-echo rsyslog/firewall checks
-echo ===================
-echo checking for ports opened by syslogd
-sudo ss -tulpn|grep syslogd
-sudo ufw status
-echo ===================
-
-echo logs being captured on loghost
-echo ===================
-for h in loghost webhost nmshost proxyhost mailhost pfsense; do echo "$h $(sudo grep -aicw $h /var/log/syslog)"; done
-echo "distinct hostnames in SystemEvents table: "
-sudo mysql -u root <<< "select distinct FromHost, count(*) from Syslog.SystemEvents group by FromHost;"
-echo ===================
