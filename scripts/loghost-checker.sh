f3e339e5f0b9d1dcae7fa1ca00779e5e4fc389f3
Dennis Simpson <dennis@zonzorp.net>
2025-05-31 10:09:32 -0400

    Create loghost-checker.sh

---
+++ scripts/loghost-checker.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+#############
+# defaults
+#############
+
+labnum="123456789"
+firstname="$FIRSTNAME"
+lastname="$LASTNAME"
+studentnumber="$STUDENTNUMBER"
+score=0
+labscore=0
+labmaxscore=0
+maxscore=0
+labscoresURL="https://zonzorp.net/gc/postlabscores.php"
+datetime=$(date +"%Y-%m-%d@%H:%M:%S%p")
+logfile="/tmp/sc$datetime$$.log"
+course="COMP1071"
+case `date +%m` in
+01|02|03|04) semester="W`date +%y`";;
+05|06|07|08) semester="S`date +%y`";;
+09|10|11|12) semester="F`date +%y`";;
+esac
+skipUpdate="no"
+ufwAlwaysOn="yes"
+. /etc/os-release
+
+# add in functions that are helpful
+
+source nets1037-funcs.sh
+source nets1037-grading-funcs.sh
+
+############
+# Main
+############
+
+# start a new logfile and start it with date/time info
+date +"server-check running on %Y-%M-%D at %H:%M %p" >$logfile
+echo "$0 $@" >>$logfile
+
+#Checks if your userID is root
+sudo-check
+
+# test if internet is reachable
+ping -c 1 8.8.8.8 >&/dev/null
+if [ $? -ne 0 ]; then
+	problem-report "Not connected to the internet. This script requires a functional IPV4 internet connection."
+	problem-report "Check that you are getting dhcp service on your first network interface (try 'ip a')."
+	problem-report "Check that you have internet service on your host computer (try 'ping 8.8.8.8' in a command window on the host computer)"
+	# leave the logfile in place for troubleshooting purposes
+	exit 1
+fi
+
+verbose="no"
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-l | --lab)
+			labnum="$2"
+			shift
+			;;
+		-s )
+			skipUpdate="yes" # this hidden options skips checking for script updates
+			;;
+		-f )
+			ufwAlwaysOn="no" # this hidden option allows not checking ufw rules for all services
+			;;
+		-v )
+			verbose="yes"
+			;;
+		*)
+			if [ "$firstname" = "" ]; then
+				firstname="$1"
+			elif [ "$lastname" = "" ]; then
+				lastname="$1"
+			elif [ "$studentnumber" = "" ]; then
+				studentnumber="$1"
+			else
+				usage
+				rm $logfile # this logfile is pointless, discard it
+				exit
+			fi
+			;;
+	esac
+	shift
+done
+
+if [ "$skipUpdate" = "no" ]; then
+	echo "Checking if script is up to date, please wait"
+ script=loghost-checker.sh
+	wget -nv -O $script-new https://zonzorp.github.io/NETS1037/raw/main/scripts/$script >& /dev/null
+	diff $script $script-new >& /dev/null
+	if [ "$?" != "0" -a -s $script-new ]; then
+		mv $script-new $script
+		chmod +x $script
+		echo "$script updated"
+		$script -s "$@"
+		rm $logfile # this logfile is pointless, discard it
+		exit
+	else
+		rm $script-new
+	fi
+fi
+
+cat <<EOF
+This script will check various parts of your server to see if you have completed
+the setup of the various services and configuration as instructed during the semester.
+***********************!!!!!!!!!!*********************
+It is expected that you use lower case only whenever you use your name as part of
+your server configuration, for username, domain name, etc.
+***********************!!!!!!!!!!*********************
+EOF
+
+while [ "$firstname" = "" ]; do
+	read -p "Your first name? " firstname
+done
+while [ "$lastname" = "" ]; do
+	read -p "Your last name? " lastname
+done
+while [ "$studentnumber" = "" ]; do
+	read -p "Your student number? " studentnumber
+done
+
+if [ $(wc -c <<< "$studentnumber") -eq 9 ]; then
+	snum=`cut -c 4-8 <<< "$studentnumber"`
+elif [ $(wc -c <<< "$studentnumber") -eq 10 ]; then
+	snum=`cut -c 5-9 <<< "$studentnumber"`
+elif [ $(wc -c <<< "$studentnumber") -eq 2 ]; then
+        snum=""
+else
+	problem-report "Your student number should be either 8 digits or 9 digits long"
+	# leave the logfile in place for troubleshooting
+	exit
+fi
+firstname=`tr 'A-Z' 'a-z'<<<"$firstname"`
+lastname=`tr 'A-Z' 'a-z'<<<"$lastname"`
+mydomain="$lastname$snum"
+arch=`arch`
+if [ $arch = "armv6l" -o $arch = "armv7l" ]; then
+	arch=armhf
+	hosttype=pi
+elif [ $arch = "i686" -o $arch = "i586" -o $arch = "x86_64" ]; then
+	arch=amd64
+	hosttype=pc
+elif [ $arch = "armv8" -o $arch = "aarch64" ]; then
+	hosttype=mac
+fi
+hostname="$hosttype$studentnumber"
+
+# Display runtime info
+verbose-report "Course/Semester: $course/$semester" 
+verbose-report "First name: $firstname"
+verbose-report "Last name : $lastname"
+verbose-report "Student Number: $studentnumber"
+verbose-report "Host name: $hostname"
+
