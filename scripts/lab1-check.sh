ad82fcdc513356142655af472bbf4de83a8877f2
zonzorp <dennis@zonzorp.net>
2026-02-23 12:08:51 -0500

    Update lab1-check.sh

--- scripts/lab1-check.sh
+++ scripts/lab1-check.sh
@@ -13,16 +13,16 @@ score=0
 
 if ping -c 1 pfsense >/dev/null; then
     echo "pfsense answers ping"
-    ((score++))
+    ((score+=3))
     if ssh admin@pfsense true >/dev/null; then
 	echo " and responds to ssh"
-        ((score+=3))
+        ((score+=2))
     else
         echo " but does not respond to ssh"
     fi
     if ssh admin@pfsense -- ping -c 1 google.com >/dev/null; then
 	echo " and can ping google"
-        ((score+=2))
+        ((score+=5))
     else
         echo " but cannot ping google"
     fi
@@ -31,19 +31,19 @@ else
 fi
 
 for host in loghost mailhost webhost proxyhost nmshost; do
-    if ping -c 1 pfsense >/dev/null; then 
+    if ping -c 1 $host >/dev/null; then
     echo "$host answers ping"
-    ((score++))
+    ((score+=6))
     if ssh $host true >/dev/null; then
         echo " and responds to ssh"
-        ((score+=3))
+        ((score+=5))
     else
         echo " but does not respond to ssh"
     fi
-    if ssh $host -- ping -c 1 google.com >/dev/null; then 
+    if ssh $host -- ping -c 1 google.com >/dev/null; then
         echo " and can ping google"
-        ((score+=2))
-    else 
+        ((score+=8))
+    else
         echo " but cannot ping google"
     fi
 else
