60b25eaad2f404566188e250f669da954d257e3c
zonzorp <dennis@zonzorp.net>
2026-03-10 17:22:39 -0400

    Update Lab02-loghost-checks.sh

--- scripts/Lab02-loghost-checks.sh
+++ scripts/Lab02-loghost-checks.sh
@@ -3,30 +3,42 @@
 username="$(id -un)"
 mid="$(hostnamectl |grep -i machine)"
 
-echo "
+if ! mysql -V 2>/dev/null; then
+    echo "MySQL not installed, not checking lab"
+    exit 1
+fi
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo "do not run this script as root"
+  exit 1
+else
+  sudo echo "
 Report for $mid by $username
 $(date)
 ================
-"
+" || exit 1
+fi
 
-score=0
+writingscore=0
+listeningscore=0
+receivingscore=0
 
 mysqlrecordcount=$(sudo mysql -u root  <<< 'select count(*) from Syslog.SystemEvents;'|tail -1)
 if [ "$mysqlrecordcount" ] && [ "$mysqlrecordcount" -gt 0 ]; then
   echo "mysql db has SystemEvents records"
-  ((score+=3))
+  ((writingscore+=15))
 else
     echo "SystemEvents table is empty"
 fi
 if sudo ss -tulpn |grep -q 'udp.*0.0.0.0:514.*0.0.0.0:.*syslogd' ; then
   echo "rsyslog is listening to the network on 514/udp"
-  ((score+=3))
+  ((listeningscore+=15))
 else
   echo "rsyslog is not listening to 514/udp for syslog on the network"
 fi
 if sudo ufw status 2>&1 |grep '514/udp.*ALLOW'; then
 	echo "ufw allows 514/udp"
-  ((score+=2))
+  ((listeningscore+=15))
 else
   echo "UFW is not allowing syslog traffic on 514/udp"
 fi
@@ -34,18 +46,19 @@ fi
 hostsinsyslog="$(sudo awk '{print $2}' /var/log/syslog|sort|uniq -c)"
 hostsindb="$(sudo mysql -u root <<< 'select distinct FromHost, count(*) from Syslog.SystemEvents group by FromHost;')"
 for host in loghost mailhost webhost proxyhost nmshost; do
-  if sudo grep -aicwq $host /var/log/syslog; then 
+  if grep -aicwq $host <<< "$hostsinsyslog"; then
     echo "$host found in /var/log/syslog"
-    ((score++))
+    ((receivingscore+=4))
+    [ $receivingscore -eq 4 ] && ((writingscore+=15))
   else
     echo "$host not found in /var/log/syslog"
   fi
 #  if [ $(sudo mysql -u root <<< 'select count(*) from Syslog.SystemEvents where FromHost like ${host}%;'|tail -1) -gt 0 ]; then
   if echo "$hostsindb" |grep -qw $host ; then
     echo "$host has records in the SystemEvents table"
-    ((score+=3))
+    ((receivingscore+=4))
   else
         echo "$host not found in the SystemEvents table"
   fi
 done
-echo "Score: $score"
+echo "Scores: writing: $writingscore, listening: $listeningscore, receiving: $receivingscore"
